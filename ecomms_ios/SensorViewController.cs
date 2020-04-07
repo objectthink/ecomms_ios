ee55ddda1e2e633052b8cded111d2b34349849d4
stephen eshelman <objectthink@comcast.net>
2020-04-06 20:42:47 -0400

    add some fields to sensor view controller

--- ecomms_ios/SensorViewController.cs
+++ ecomms_ios/SensorViewController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using UIKit;
 
 namespace ecomms_ios
@@ -22,6 +21,13 @@ namespace ecomms_ios
             // Perform any additional setup after loading the view, typically from a nib.
 
             Title = sensor.name;
+            _name.Text = sensor.name;
+            _location.Text = sensor.location;
+
+            //json
+            
+            //parse description json...
+            _temperature.Text = "coming!";
         }
 
         public override void DidReceiveMemoryWarning()
--- ecomms_ios/SensorViewController.designer.cs
+++ ecomms_ios/SensorViewController.designer.cs
@@ -1,20 +1,30 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
 using Foundation;
 using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace ecomms_ios
 {
-    [Register("SensorViewController")]
+    [Register ("SensorViewController")]
     partial class SensorViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        UIKit.UITextField _location { get; set; }
+
+
+        [Outlet]
+        UIKit.UITextField _name { get; set; }
+
+
+        [Outlet]
+        UIKit.UITextField _temperature { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
         }
     }
